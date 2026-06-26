d0a967dfce0086e1dcb50979dd151b3280ecc081
rosarioborgesi <borgesiros@gmail.com>
2026-06-26 15:36:27 +0200

    Updated MockLendingPoolAddressProvider

--- test/mocks/MockLendingPoolAddressProvider.sol
+++ test/mocks/MockLendingPoolAddressProvider.sol
@@ -5,21 +5,30 @@ contract MockLendingPoolAddressProvider {
     error MockLendingPoolAddressProvider__AddressIsZero();
 
     address private i_lendingPool;
+    address private i_lendingPoolCore;
     address private i_lendingPoolConfigurator;
 
     constructor(address _lendingPool, address _lendingPoolConfigurator) {
-        if (_lendingPool == address(0)) {
-            revert MockLendingPoolAddressProvider__AddressIsZero();
-        }
-        if (_lendingPoolConfigurator == address(0)) {
+        if (_lendingPool == address(0) || _lendingPoolConfigurator == address(0)) {
             revert MockLendingPoolAddressProvider__AddressIsZero();
         }
         i_lendingPool = _lendingPool;
         i_lendingPoolConfigurator = _lendingPoolConfigurator;
     }
 
+    function setLendingPoolCore(address _lendingPoolCore) external {
+        if (_lendingPoolCore == address(0)) {
+            revert MockLendingPoolAddressProvider__AddressIsZero();
+        }
+        i_lendingPoolCore = _lendingPoolCore;
+    }
+
     function getLendingPool() external view returns (address) {
         return i_lendingPool;
+    }
+
+    function getLendingPoolCore() external view returns (address) {
+        return i_lendingPoolCore;
     }
 
     function getLendingPoolConfigurator() external view returns (address) {
