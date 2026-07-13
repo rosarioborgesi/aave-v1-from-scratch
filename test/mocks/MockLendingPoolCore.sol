666792753f70b0df146167dda51ef9bc92a7ab84
rosarioborgesi <borgesiros@gmail.com>
2026-07-13 16:01:09 +0200

    Updated test/mocks/MockLendingPoolCore.sol

--- test/mocks/MockLendingPoolCore.sol
+++ test/mocks/MockLendingPoolCore.sol
@@ -1,28 +1,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
+import {SafeERC20, IERC20} from "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
+
+import {EthAddressLib} from "src/libraries/EthAddressLib.sol";
+
 contract MockLendingPoolCore {
-    // Stores the return values of getReserveConfiguration()
-    struct ReserveConfiguration {
+    ////////////////////////////////
+    //            Errors          //
+    ////////////////////////////////
+
+    error MockLendingPoolCore__EthTransferFailed(address to, uint256 amount);
+
+    ///////////////////////////////////
+    //            Libraries          //
+    ///////////////////////////////////
+    using SafeERC20 for IERC20;
+
+    ////////////////////////////////
+    //      State Variables       //
+    ////////////////////////////////
+    address[] private s_reserves;
+    mapping(address reserve => bool isAdded) private s_isReserveAdded;
+    mapping(address reserve => TestReserveData data) private s_testReserveData;
+    mapping(address user => mapping(address reserve => TestUserReserveData data)) private s_testUserReserveData;
+
+    /////////////////////////////////
+    //      Type declarations      //
+    /////////////////////////////////
+
+    // Stores the reserve information used by the mock.
+    struct TestReserveData {
         uint256 decimals;
         uint256 baseLtv;
         uint256 liquidationThreshold;
         bool usageAsCollateralEnabled;
+        uint256 normalizedIncome;
+        address aTokenAddress;
+        bool isActive;
     }
 
-    // Stores the return values getUserBasicReserveData()
-    struct UserBasicReserveData {
+    // Stores the user's reserve information used by the mock.
+    struct TestUserReserveData {
         uint256 liquidityBalance;
         uint256 borrowBalance;
         uint256 originationFee;
         bool useAsCollateral;
     }
 
-    address[] private s_reserves;
-    mapping(address reserve => bool isAdded) private s_isReserveAdded;
-    mapping(address reserve => ReserveConfiguration configuration) private s_configurations;
-    mapping(address user => mapping(address reserve => UserBasicReserveData data)) private s_userData;
-    mapping(address reserve => uint256 normalizedIncome) private s_reserveNormalizedIncome;
+    ////////////////////////////////
+    //          Functions         //
+    ////////////////////////////////
+
+    receive() external payable {}
 
     function addReserve(address reserve) external {
         if (s_isReserveAdded[reserve]) {
@@ -40,12 +70,14 @@ contract MockLendingPoolCore {
         uint256 liquidationThreshold,
         bool usageAsCollateralEnabled
     ) external {
-        s_configurations[reserve] = ReserveConfiguration(
-            decimals, baseLtv, liquidationThreshold, usageAsCollateralEnabled
-        );
+        TestReserveData storage data = s_testReserveData[reserve];
+        data.decimals = decimals;
+        data.baseLtv = baseLtv;
+        data.liquidationThreshold = liquidationThreshold;
+        data.usageAsCollateralEnabled = usageAsCollateralEnabled;
     }
 
-    function setUserBasicReserveData(
+    function setTestUserReserveData(
         address user,
         address reserve,
         uint256 liquidityBalance,
@@ -53,13 +85,20 @@ contract MockLendingPoolCore {
         uint256 originationFee,
         bool useAsCollateral
     ) external {
-        s_userData[user][reserve] = UserBasicReserveData(
-            liquidityBalance, borrowBalance, originationFee, useAsCollateral
-        );
+        s_testUserReserveData[user][reserve] =
+            TestUserReserveData(liquidityBalance, borrowBalance, originationFee, useAsCollateral);
     }
 
     function setReserveNormalizedIncome(address reserve, uint256 normalizedIncome) external {
-        s_reserveNormalizedIncome[reserve] = normalizedIncome;
+        s_testReserveData[reserve].normalizedIncome = normalizedIncome;
+    }
+
+    function setReserveIsActive(address reserve, bool isActive) external {
+        s_testReserveData[reserve].isActive = isActive;
+    }
+
+    function setReserveATokenAddress(address reserve, address aTokenAddress) external {
+        s_testReserveData[reserve].aTokenAddress = aTokenAddress;
     }
 
     function getReserves() external view returns (address[] memory) {
@@ -67,14 +106,21 @@ contract MockLendingPoolCore {
     }
 
     function getReserveConfiguration(address reserve) external view returns (uint256, uint256, uint256, bool) {
-        ReserveConfiguration memory configuration = s_configurations[reserve];
+        TestReserveData memory data = s_testReserveData[reserve];
 
-        return (
-            configuration.decimals,
-            configuration.baseLtv,
-            configuration.liquidationThreshold,
-            configuration.usageAsCollateralEnabled
-        );
+        return (data.decimals, data.baseLtv, data.liquidationThreshold, data.usageAsCollateralEnabled);
+    }
+
+    function getTestReserveData(address reserve) external view returns (TestReserveData memory) {
+        return s_testReserveData[reserve];
+    }
+
+    function getTestUserReserveData(address reserve, address user)
+        external
+        view
+        returns (TestUserReserveData memory)
+    {
+        return s_testUserReserveData[user][reserve];
     }
 
     function getUserBasicReserveData(address reserve, address user)
@@ -82,16 +128,45 @@ contract MockLendingPoolCore {
         view
         returns (uint256, uint256, uint256, bool)
     {
-        UserBasicReserveData memory data = s_userData[user][reserve];
+        TestUserReserveData memory data = s_testUserReserveData[user][reserve];
 
         return (data.liquidityBalance, data.borrowBalance, data.originationFee, data.useAsCollateral);
     }
 
     function getReserveNormalizedIncome(address reserve) external view returns (uint256) {
-        return s_reserveNormalizedIncome[reserve];
+        return s_testReserveData[reserve].normalizedIncome;
     }
 
     function isUserUseReserveAsCollateralEnabled(address reserve, address user) external view returns (bool) {
-        return s_userData[user][reserve].useAsCollateral;
+        return s_testUserReserveData[user][reserve].useAsCollateral;
+    }
+
+    function getReserveATokenAddress(address reserve) external view returns (address) {
+        return s_testReserveData[reserve].aTokenAddress;
+    }
+
+    function getReserveIsActive(address reserve) external view returns (bool) {
+        return s_testReserveData[reserve].isActive;
+    }
+
+    function getReserveAvailableLiquidity(address reserve) external view returns (uint256) {
+        if (reserve == EthAddressLib.ethAddress()) {
+            return address(this).balance;
+        }
+
+        return IERC20(reserve).balanceOf(address(this));
+    }
+
+    function updateStateOnRedeem(address, address, uint256, bool) external {}
+
+    function transferToUser(address _reserve, address payable _user, uint256 _amount) external {
+        if (_reserve != EthAddressLib.ethAddress()) {
+            IERC20(_reserve).safeTransfer(_user, _amount);
+        } else {
+            (bool result,) = _user.call{value: _amount, gas: 50000}("");
+            if (!result) {
+                revert MockLendingPoolCore__EthTransferFailed(_user, _amount);
+            }
+        }
     }
 }
