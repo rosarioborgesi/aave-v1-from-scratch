d5a670a2ca28fba098fa8cf9c5f0dbfc969e577a
rosarioborgesi <borgesiros@gmail.com>
2026-07-29 15:56:32 +0200

    Added Natspec for getUserIndex

--- src/tokenization/AToken.sol
+++ src/tokenization/AToken.sol
@@ -396,6 +396,11 @@ contract AToken is ERC20 {
         return i_underlyingAssetAddress;
     }
 
+    /**
+    * @dev returns the last index of the user, used to calculate the balance of the user
+    * @param _user address of the user
+    * @return the last user index
+    **/
     function getUserIndex(address _user) external view returns (uint256) {
         return s_userIndexes[_user];
     }
